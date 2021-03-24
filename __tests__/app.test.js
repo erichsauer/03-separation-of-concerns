b13c397059a24c9acecbe01cb7fe914e9d7398ad
Erich Sauer <erich.sauer@gmail.com>
2021-03-24 16:11:26 -0700

    adds several crud routes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -19,7 +19,7 @@ describe('03_separation-of-concerns-demo routes', () => {
       .post('/api/v1/orders')
       .send({ quantity: 10 })
       .then((res) => {
-        // expect(createMessage).toHaveBeenCalledTimes(1);
+        // expect(sendSms).toHaveBeenCalledTimes(1);
         expect(res.body).toEqual({
           id: '1',
           quantity: 10,
@@ -27,14 +27,31 @@ describe('03_separation-of-concerns-demo routes', () => {
       });
   });
 
-  it('ASYNC/AWAIT: creates a new order in our database and sends a text message', async () => {
-    const res = await request(app)
-      .post('/api/v1/orders')
-      .send({ quantity: 10 });
+  it('gets all orders from the database', () => {
+    request(app).post('/api/v1/orders').send({ quantity: 10 }).then();
 
-    expect(res.body).toEqual({
-      id: '1',
-      quantity: 10,
-    });
+    return request(app)
+      .get('/api/v1/orders')
+      .then((res) => {
+        expect(res.body).toEqual([
+          {
+            id: '1',
+            quantity: 10,
+          },
+        ]);
+      });
+  });
+
+  it('gets one order from the database by id', () => {
+    request(app).post('/api/v1/orders').send({ quantity: 10 }).then();
+
+    return request(app)
+      .get('/api/v1/orders/1')
+      .then((res) => {
+        expect(res.body).toEqual({
+          id: '1',
+          quantity: 10,
+        });
+      });
   });
 });
