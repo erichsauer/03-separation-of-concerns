e031380899a3a2a7d0dd21868d2b983c27b53712
Erich Sauer <erich.sauer@gmail.com>
2021-03-24 18:56:08 -0700

    adds more tested endpoints (sorry for the scarse commit history!)

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,16 +2,38 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Order = require('../lib/models/Order');
 
-jest.mock('twilio', () => () => ({
-  messages: {
-    create: jest.fn(),
-  },
-}));
+jest.mock('../lib/utils/twilio.js');
+const twilio = require('../lib/utils/twilio');
 
 describe('03_separation-of-concerns-demo routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  let order;
+  beforeEach(async () => {
+    order = await Order.insert({ quantity: 10 });
+
+    twilio.sendSms.mockClear();
+  });
+
+  it('sends a sms upon new order', () => {
+    return request(app)
+      .post('/api/v1/orders')
+      .send({ quantity: 10 })
+      .then(() => {
+        expect(twilio.sendSms).toHaveBeenCalledTimes(1);
+      });
+  });
+
+  it('sends a sms upon deleted order', () => {
+    return request(app)
+      .delete('/api/v1/orders/1')
+      .then(() => {
+        expect(twilio.sendSms).toHaveBeenCalledTimes(1);
+      });
   });
 
   it('creates a new order in our database and sends a text message', () => {
@@ -19,16 +41,15 @@ describe('03_separation-of-concerns-demo routes', () => {
       .post('/api/v1/orders')
       .send({ quantity: 10 })
       .then((res) => {
-        // expect(sendSms).toHaveBeenCalledTimes(1);
         expect(res.body).toEqual({
-          id: '1',
+          id: '2',
           quantity: 10,
         });
       });
   });
 
-  it('gets all orders from the database', () => {
-    request(app).post('/api/v1/orders').send({ quantity: 10 }).then();
+  it('gets all orders from the database', async () => {
+    await request(app).post('/api/v1/orders').send({ quantity: 5 });
 
     return request(app)
       .get('/api/v1/orders')
@@ -38,20 +59,42 @@ describe('03_separation-of-concerns-demo routes', () => {
             id: '1',
             quantity: 10,
           },
+          {
+            id: '2',
+            quantity: 5,
+          },
         ]);
       });
   });
 
-  it('gets one order from the database by id', () => {
-    request(app).post('/api/v1/orders').send({ quantity: 10 }).then();
+  it('gets one order from the database by id', async () => {
+    return request(app)
+      .get(`/api/v1/orders/${order.id}`)
+      .then((res) => {
+        expect(res.body).toEqual(order);
+      });
+  });
+
+  it('updates one order from the database by id', async () => {
+    await request(app).put(`/api/v1/orders/${order.id}`).send({ quantity: 5 });
 
     return request(app)
-      .get('/api/v1/orders/1')
+      .get(`/api/v1/orders/${order.id}`)
       .then((res) => {
         expect(res.body).toEqual({
           id: '1',
-          quantity: 10,
+          quantity: 5,
         });
+      });
+  });
+
+  it('deletes one order from the database by id', async () => {
+    await request(app).delete(`/api/v1/orders/${order.id}`);
+
+    return request(app)
+      .get(`/api/v1/orders/`)
+      .then((res) => {
+        expect(res.body).toEqual([]);
       });
   });
 });
